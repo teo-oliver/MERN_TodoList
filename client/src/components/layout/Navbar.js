d6b98886b6a000b0ddb1da2294aa14847a875f62
Teo Oliver <teo.liz.oli@gmail.com>
2019-08-06 13:32:13 +0100

    v1 deployed

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,24 +5,19 @@ function Navbar() {
   return (
     <div>
       <nav className="navbar">
-        <Link class="navbar-brand navbar-link" to="/">
-          Mern_Todo
-        </Link>
-        <div class="d-flex justify-content-end">
-          <Link class="navbar-brand navbar-link" to="/app">
-            TodoList
-          </Link>
+        <div class="navbar-brand">Todo_List</div>
 
+        <div class="d-flex justify-content-end">
           <a
             class="navbar-brand navbar-link"
-            href="https://github.com/teo-oliver"
+            href="https://github.com/teo-oliver/MERN_TodoList"
             target="_blank"
           >
             GitHub
           </a>
           <a
             class="navbar-brand navbar-link"
-            href="https://www.linkedin.com/in/teo-oliver-84b33185/"
+            href="https://teo-oliver.github.io/"
             target="_blank"
           >
             About
